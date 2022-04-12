34f981697ed1fdc18e0f1076536358c00cabada7
luisedu99 <luis.santos748@yahoo.com>
2022-04-12 02:19:38 -0600

    Initial commit

---
+++ app/src/main/java/sv/edu/udb/desafio2pm190339cs171609/MainActivity.java
@@ -0,0 +1,14 @@
+package sv.edu.udb.desafio2pm190339cs171609;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
